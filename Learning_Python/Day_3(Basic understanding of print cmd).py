a5d98ec3f848b19d6b37f74ff1a2ec13b32a3ec0
vinayak goyal <vinayakgoyalwork16@gmail.com>
2026-08-13 00:57:34 +0530

    Create Day_3(Basic understanding of print cmd).py

---
+++ Learning_Python/Day_3(Basic understanding of print cmd).py
@@ -0,0 +1,6 @@
+#print is a function that is used to print the output on the screen. Only if it is in paranthesis. 
+#print(Hello world) will give error because Hello world is not in double quotes so it is not a string and python dont know what it is, python works with strings, numbers and other data types. for number we dont have to put in quotes. 
+print("Hello world") 
+print("5+5") #it will print 5+5 because it is in double quotes so it is a string.
+print(5+5 , 1) #it will print 10 because it is not in double quotes so it is a number and python can do the calculation.
+print("5"+"5") #it will print 55 because it is in double quotes so it is a string and python will concatenate the two strings.
