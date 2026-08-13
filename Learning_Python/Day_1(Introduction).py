51032e634a92e0f70c55742cbe20ebec7a1bc893
vinayak goyal <vinayakgoyalwork16@gmail.com>
2026-08-13 23:57:48 +0530

    Create Day_1(Introduction).py

---
+++ Learning_Python/Day_1(Introduction).py
@@ -0,0 +1,12 @@
+print("Hello world")
+print(10 + 5)
+print(10 - 5)
+print(10 * 5) #product
+print(23 / 5) #division
+print(23 // 5) #floor division(gif)
+print(23 % 5) #remainder
+print(2 ** 3) #exponent
+print(10 > 5) #greater than
+print(10 < 5) #less than
+print(10 == 5) #equal to
+print(10 != 5) #not equal to
