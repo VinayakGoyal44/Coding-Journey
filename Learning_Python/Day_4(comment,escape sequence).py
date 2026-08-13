d16f139bfafb2801ca6c4d33e513a0af2d27564c
vinayak goyal <vinayakgoyalwork16@gmail.com>
2026-08-14 00:30:02 +0530

    Create Day_4(comment,escape sequence).py

---
+++ Learning_Python/Day_4(comment,escape sequence).py
@@ -0,0 +1,7 @@
+#this is comment. Comment is used to explain code to yourself or any other who is opening the source code without interfying with main code. 
+# we can comment out single line using # symbol and multiple lines using ctrl + / or using """ """. 
+#to insert character which cant be directly insert in string we use escape sequence characters.ex- new line by spacebar. 
+#to insert new line in string we use \n and to insert tab space we use \t and for double quote we use \".
+print("hello world\nthis is \"python programming\"\tlanguage")
+print("hey",5,7, sep="-", end=".\n") #anything can be used in seperator it used to join multiple entities. end is used to insert any character at the end of print statement. by default it is new line.
+print("hlo")
