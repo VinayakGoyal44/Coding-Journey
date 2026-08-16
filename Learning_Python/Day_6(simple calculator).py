44152a57fa3e3526d055e42e592d7b82e45578fc
vinayak goyal <vinayakgoyalwork16@gmail.com>
2026-08-17 00:37:48 +0530

    Create Day_6(simple calculator).py

---
+++ Learning_Python/Day_6(simple calculator).py
@@ -0,0 +1,6 @@
+a=10
+b=4
+print("Addition of",a,"and",b,"is:",a+b)
+print("Substraction of",a,"and",b,"is:",a-b)
+print("Multiplication of",a,"and",b,"is:",a*b)
+print("Divison of",a,"and",b,"is:",a/b)
