bbf2c1e2aebd7424852adae58d43582a5a866b1e
vinayak goyal <vinayakgoyalwork16@gmail.com>
2026-08-18 00:50:27 +0530

    Create Day_7(Typecasting).py

---
+++ Learning_Python/Day_7(Typecasting).py
@@ -0,0 +1,9 @@
+#Typecasting: the conversion of one data type into another data type is known as typecasting. 
+#int(),float(),str(),ord(),hex(),oct(),tuple(),set(),list(),dict(),etc are methods for typecasting
+a="1"
+b="2"
+print(int(a)+int(b)) #this is explicit conversion. the data is converted ater giving command. 
+#implicit conversion
+c=1
+d=2.9
+print(c+d) #the data is converted from integer to float without giving command,, this is implicit typecasting.
