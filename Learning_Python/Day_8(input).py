da1e274120c6273c67c8b0c2e35d059fad948481
vinayak goyal <vinayakgoyalwork16@gmail.com>
2026-08-18 00:50:34 +0530

    Create Day_8(input).py

---
+++ Learning_Python/Day_8(input).py
@@ -0,0 +1,13 @@
+# a=input("Enter your name:") #used to take input from user. Gives return value as string to convert in other data type we have to typecast it. 
+# print("My Name is",a)
+c=input(" Enter first number: ")
+d=input(" Enter second number: ")  
+print(c+d)  #printed as string                                      
+# b=int(input())
+# print(b)
+print(int(c)+int(d))        
+print(int(c)-int(d))        
+print(int(c)*int(d))        
+print(int(c)/int(d))        
+print(int(c)//int(d))        
+print(int(c)%int(d))        
