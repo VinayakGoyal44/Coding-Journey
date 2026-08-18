628333cc51ac1d99fc877f16d445327433623a62
vinayak goyal <vinayakgoyalwork16@gmail.com>
2026-08-19 00:26:18 +0530

    Create Day_9(strings).py

---
+++ Learning_Python/Day_9(strings).py
@@ -0,0 +1,13 @@
+#In python anything enclosed in quotes is considered a string. Strings can be defined using single quotes (' '), double quotes (" "), (''' ''') or (""" """). Strings can be defined in multiple lines using triple quotes.
+apple='"He wants to eat an apple"'
+print(apple)
+#In python strings are like array of characters. 
+name= "vinayak"
+print(name[0]) #prints first character of string 
+print(name[1]) #prints second character of string 
+print(name[2]) #prints third character of string 
+print("lets use a for loop")
+for character in name: #prints all characters of string one by one
+    print(character)
+for character in apple:
+    print(character)
