1e6524ef3e7571d47fdc6c455e211ffd5ae96ded
vinayak goyal <vinayakgoyalwork16@gmail.com>
2026-08-20 00:21:18 +0530

    Create Day_10(string slicing).py

---
+++ Learning_Python/Day_10(string slicing).py
@@ -0,0 +1,6 @@
+names="Vinayak,Ankit"
+print(names[0:7]) #print characters upto index n-1 (not including 7 but including 0)
+print(len(names)) #print length of string
+print(names[:5]) #print characters from index 0 to 4
+print(names[:-3])#python automatically makes it print(names[0:len(names)-3])
+print(names[-8:-1])#[len(names)-8:len(names)-1]
